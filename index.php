1526ebecf6005a974971c8ac178832329c9c0c33
MARLON GONZALEZ <maggww77@gmail.com>
2020-11-28 17:55:22 -0600

    Condicional Switch, While and For

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
         </div>
         <div class="row">
             <div class="col-12">
-                <div class="p-3 m-2 bg-primary text-white">
+                <div class="p-3 m-2 bg-info text-white">
                     <h3>Clase: Operadores</h3> <br>
                     <?php
                     $num1 = 500;
@@ -65,10 +65,52 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="p-3 m-2 bg-info text-white">
+                    <h3>Clase: Condicional Switch</h3> <br>
+                    <?php
+                    $option = "Marlon";
+                    switch ($option) {
+                        case "Marlon":
+                            echo "Escogiste la mejor opcion";
+                            break;
+                        default:
+                            echo "no escogiste una buena opcion";
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="p-3 m-2 bg-success text-white">
+                    <h3>Clase: Bucle While</h3> <br>
+                    <?php
+                    $optionn = 5;
+                    while($optionn <=10) {
+                        echo "El numero es: " .$optionn . "<br>";
+                        $optionn++;
+                    }
+                    
+                    ?>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="p-3 m-2 bg-warning text-white">
+                    <h3>Clase: Bucle For</h3> <br>
+                    <?php
+                    $optionn = 5;
+                    for ($i=0; $i<=$optionn;$i++) {
+                         echo "El numero: " . $i . "<br>";
+
+                    }
+                    
+                    ?>
+                </div>
+            </div>
+        </div>
     </div>
-
-
-
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
